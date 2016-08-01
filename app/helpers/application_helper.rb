aefac0933e76f4f881af0f4bb6eba9f135f232e5
empireofryan <rjohnson2001@gmail.com>
2016-07-31 21:53:12 -0400

    gravatar working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_id = 'https://www.gravatar.com/avatar/#test@testtest.com.jpg?d=identical&s=150'
     if user.image?
       user.image
     else
-    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+      'https://www.gravatar.com/avatar/#test@testtest.com.jpg?d=identical&s=150'
+    # "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     end
   end
 end
