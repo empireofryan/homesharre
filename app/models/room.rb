7a89a476771ca121bceb288804b5c37b075d80e0
empireofryan <rjohnson2001@gmail.com>
2016-12-28 16:43:33 -0500

    review model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
@@ -9,4 +10,7 @@ class Room < ActiveRecord::Base
   validates :home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, presence: true
   validates :listing_name, :summary, length: {maximum: 500}
 
+  def average_rating
+    reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+  end
 end
